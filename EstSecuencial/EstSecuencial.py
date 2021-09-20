f1214cd3e488da4aabd198b908a1da0eef6f4a91
katie493 <90202094+katie493@users.noreply.github.com>
2021-09-20 16:22:43 +0000

    pull

---
+++ EstSecuencial/EstSecuencial.py
@@ -0,0 +1,8 @@
+#Datos de entrada
+print("Hallar el area de un rombo")
+DM=int(input("Ingrese diagonal mayor:"))
+dm=int(input("Ingrese diagonal menor:"))
+#proceso
+area=(DM*dm)/2
+#Datos de salida
+print("El area del rombo es:", area, "mt2")
